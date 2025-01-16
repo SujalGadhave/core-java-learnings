c7d7c29bae7f71f57540feca0676f4758a2a35ff
admin <admin@Sujal>
2025-01-16 10:49:03 +0530

    Initial commit

---
+++ src/org/dnyanyog/Teacher.java
@@ -0,0 +1,35 @@
+package org.dnyanyog;
+
+public class Teacher {
+	private String fisrtName;
+	private String lastName;
+	private String email;
+	private String mobile;
+	
+	private Address address = new Address();
+	
+	public String getFisrtName() {
+		return fisrtName;
+	}
+	public void setFisrtName(String fisrtName) {
+		this.fisrtName = fisrtName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+}
